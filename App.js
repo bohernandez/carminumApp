7c575864401155ca501d8c6720b4dcba9e8343d7
bohernandez <boris.hceron@gmail.com>
2018-07-19 23:37:14 -0600

    Running on android device

--- App.js
+++ App.js
@@ -5,9 +5,10 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Agenda Misa de Ordenacion Sacerdotal</Text>
+        <Text>Angel Aleman</Text>
+        <Text>E. Canta Iglesia</Text>
+        <Text>  Pueblo de Reyes</Text>
       </View>
     );
   }
