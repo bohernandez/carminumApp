ac5962a4caed78a8f27bbdc4a0b3ce27f8f2cb3e
bohernandez <boris.hceron@gmail.com>
2018-07-25 21:57:47 -0600

    Adding pdf extension to project

--- app/components/lyrics.js
+++ app/components/lyrics.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Pdf from 'react-native-pdf';
+
 class Lyrics extends React.Component {
     static navigationOptions = {
       title: 'Songs',
@@ -7,10 +9,24 @@ class Lyrics extends React.Component {
     render() {
       const { navigate } = this.props.navigation;
       const { name, lyric } = this.props.navigation.state.params;
+      const source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
+
       return (
         <View style={StyleSheet.container}>
           <Text>Title {name}</Text>
           <Text>Letra {lyric}</Text>
+          <Pdf
+                    source={source}
+                    onLoadComplete={(numberOfPages,filePath)=>{
+                        console.log(`number of pages: ${numberOfPages}`);
+                    }}
+                    onPageChanged={(page,numberOfPages)=>{
+                        console.log(`current page: ${page}`);
+                    }}
+                    onError={(error)=>{
+                        console.log(error);
+                    }}
+                    style={styles.pdf} />
         </View>
       );
     }
@@ -23,6 +39,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pdf: {
+    flex:1,
+    width:Dimensions.get('window').width,
+  }
 });
 
 export default Lyrics;
